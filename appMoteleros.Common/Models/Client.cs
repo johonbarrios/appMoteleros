05ea6bc81f58113ef031e09851d375510507af2d
johonbarrios <arrebol6013@gmail.com>
2019-02-19 16:32:43 -0500

    Primer Modelo Common

---
+++ appMoteleros.Common/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿namespace appMoteleros.Common.Models
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Client
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = false)]
+        public Decimal Price { get; set; }
+    }
+}
